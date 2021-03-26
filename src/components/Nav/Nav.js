f735c2b06986ba1e39f57853364a3da0e7819075
nrsimonelli <nrsimonelli@gmail.com>
2021-03-26 00:21:43 -0500

    nav styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,12 @@ const Nav = () => {
   return (
     <div className='nav-root'>
       <div className='container'>
-        <div className='content'>Nav content</div>
+        <div className='title h3'>App Title</div>
+        <div className='sub-container h5'>
+          <div>logo</div>
+          <div>more</div>
+          <div className='link'>link</div>
+        </div>
       </div>
     </div>
   );
