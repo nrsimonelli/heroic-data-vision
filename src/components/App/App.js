23fa0a30e0ad663b6cebe629dbfcb5ba58a3c2dc
nrsimonelli <nrsimonelli@gmail.com>
2021-03-25 20:55:44 -0500

    npx create and removed extra boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function App() {
+  return (
+    <div className='root'>
+      <div>content</div>
+    </div>
+  );
+}
+
+export default App;
