391a949cba115ca7d8953ec1f06556c5ecf061ab
nrsimonelli <nrsimonelli@gmail.com>
2021-04-03 18:59:06 -0500

    removed static data references from hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,16 +17,7 @@ class Hero extends Component {
     }
   }
 
-  state = {
-    showEgg: false,
-  };
-
-  delayedState = () => {
-    this.setState({ showEgg: true });
-  };
-
   render() {
-    const hero = this.props.data;
     const heroId = this.props.heroId;
     const egg = this.props.egg;
     const eggData = [];
@@ -39,64 +30,46 @@ class Hero extends Component {
 
       await heroId(newHeroId);
       eggData.push(egg);
-      this.delayedState();
     };
 
     const prevClicked = () => {
       let newHeroId = 731;
-      const trueEgg = this.state.showEgg;
-      if (trueEgg && egg.id > 1) {
+      if (egg.id > 1) {
         newHeroId = egg.id - 1;
       }
-      if (!trueEgg && hero.id > 1) {
-        newHeroId = hero.id - 1;
-      }
-      if (trueEgg && egg.id === 1) {
+
+      if (egg.id === 1) {
         newHeroId = 731;
       }
-      if (!trueEgg && hero.id === 1) {
-        newHeroId = 731;
-      }
+
       heroId(newHeroId);
       eggData.push(egg);
-      this.delayedState();
     };
 
     const nextClicked = () => {
       let newHeroId = 1;
-      const trueEgg = this.state.showEgg;
-      if (trueEgg && egg.id < 731) {
+      if (egg.id < 731) {
         newHeroId = Number(egg.id) + 1;
       }
-      if (!trueEgg && hero.id < 731) {
-        newHeroId = Number(hero.id) + 1;
-      }
-      if (trueEgg && egg.id === 731) {
+
+      if (egg.id === 731) {
         newHeroId = Number(1);
       }
-      if (!trueEgg && hero.id === 731) {
-        newHeroId = Number(1);
-      }
+
       eggData.push(egg);
       heroId(newHeroId);
-      this.delayedState();
     };
+
     return (
       <div className='image-primary'>
         <div className='main-header-container'>
-          <div className='title'>
-            {this.state.showEgg ? egg.name : hero.name}
-          </div>
+          <div className='title'>{egg.name}</div>
         </div>
         <div
           className='image'
-          style={
-            !this.state.showEgg
-              ? { backgroundImage: `url(${hero.image.url})` }
-              : {
-                  backgroundImage: `url(${egg.image.url})`,
-                }
-          }
+          style={{
+            backgroundImage: `url(${egg.image.url})`,
+          }}
         ></div>
 
         <div className='hero-select'>
@@ -118,7 +91,6 @@ class Hero extends Component {
 }
 
 const mapReduxStateToProps = (reduxState) => ({
-  hero: reduxState.heroReducer,
   eggOne: reduxState.eggOneReducer,
   eggTwo: reduxState.eggTwoReducer,
 });
